351318dc59ed8c11336e0a064ab8389003e2dee5
Soneta <sopha.sts86@gmail.com>
2024-04-01 17:50:08 +0300

    Create database

--- CheckingDocx/Program.cs
+++ CheckingDocx/Program.cs
@@ -1,4 +1,10 @@
+using Core;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string? connStr = builder.Configuration.GetConnectionString("LocalDb");
 
 // Add services to the container.
 
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext(connStr);
+
+builder.Services.AddValidators();
 
 var app = builder.Build();
 
---
+++ Core/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class ServiceExtensions
+    {
+        public static void AddValidators(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+        }
+    }
+}
