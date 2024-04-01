8901b004e404f158f3b6e22188da353559c97c5c
Soneta <sopha.sts86@gmail.com>
2024-04-01 14:00:14 +0300

    Create entities

---
+++ Core/Entities/Value.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Value
+    {
+        public uint Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Requirement Requirement { get; set; }
+    }
+}
