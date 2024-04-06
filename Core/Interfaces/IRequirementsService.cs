9f1ff8d6a1843e36ec1db1a9503634efdfc433e6
Soneta <sopha.sts86@gmail.com>
2024-04-06 12:15:42 +0300

    Create interfaces

---
+++ Core/Interfaces/IRequirementsService.cs
@@ -0,0 +1,21 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IRequirementsService
+    {
+        List<Requirement> GetRequirements();
+
+        void Create(RequirementDTO requirement);
+
+        void Edit(Requirement requirement);
+
+        void Delete(int id);
+    }
+}
---
+++ Core/Interfaces/IValuesService.cs
@@ -0,0 +1,21 @@
+﻿using Core.DTOs;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IValuesService
+    {
+        List<Value> GetValues();
+
+        void Create(ValueDTO value);
+
+        void Edit(Value value);
+
+        void Delete(int id);
+    }
+}
